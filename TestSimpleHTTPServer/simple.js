2b9f102634517386df55f520c4cc245d9e3205c7
Kassiopeia14 <kasiananstasiia@gmail.com>
2022-07-08 17:15:33 +0100

    simple.js works

---
+++ TestSimpleHTTPServer/simple.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello Stanford!\n");
+});
+server.listen(3000);
